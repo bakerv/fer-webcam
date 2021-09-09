a63f9cf0d0722610668b543f6f3d71d073a738dc
bakerv <80018640+bakerv@users.noreply.github.com>
2021-09-09 16:07:47 -0500

    confirmed frame rate limit to be the issue, failed at 11. Trying with 15

--- client.js
+++ client.js
@@ -157,15 +157,15 @@ function start() {
                 width: parseInt(resolution[0], 0),
                 height: parseInt(resolution[1], 0),
                 frameRate: {
-                    ideal: 11,
-                    max: 11
+                    ideal: 15,
+                    max: 15
                 }
             };
         } else {
             constraints.video = {  
                 frameRate: {
-                    ideal: 11,
-                    max: 11
+                    ideal: 15,
+                    max: 15
             }};
         }
     }
