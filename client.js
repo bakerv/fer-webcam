ff2b2aec59bde3b56587c3d6c54c47060b207bc1
bakerv <80018640+bakerv@users.noreply.github.com>
2021-09-09 15:51:57 -0500

    raising to 30 resolved issues

--- client.js
+++ client.js
@@ -157,15 +157,15 @@ function start() {
                 width: parseInt(resolution[0], 0),
                 height: parseInt(resolution[1], 0),
                 frameRate: {
-                    ideal: 30,
-                    max: 30
+                    ideal: 11,
+                    max: 11
                 }
             };
         } else {
             constraints.video = {  
                 frameRate: {
-                    ideal: 30,
-                    max: 30
+                    ideal: 11,
+                    max: 11
             }};
         }
     }
